276ffd5feb491e53db1180406de748a60e43dbd1
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-11-06 23:00:35 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+
+const SelectedImage = ({ img }) => {
+  return <div>Selected sticker img: {img}</div>;
 };
+
+export class App extends Component {
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+};
+
+  handleSelect = stickerImg => {
+    this.setState({ selectedSticker: stickerImg });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Example 3</h1>
+        <SelectedImage img={this.state.selectedSticker} />
+        <StickerList
+          stickers={stickers}
+          onSelect={this.handleSelect}
+          selectedSticker={this.state.selectedSticker}
+        />
+      </>
+    );
+  }
+}
