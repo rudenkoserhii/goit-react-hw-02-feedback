0ea2a35beb0c90d7fb435846b8a404f5c030e0ac
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-11-10 15:00:30 +0200

    made fixing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,39 +5,38 @@ import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
 export class App extends Component {
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-onLeaveFeedback = (e) => {
-  const updates = {};
-  updates[String(e.target.textContent)] = Number(this.state[e.target.textContent] + 1);
-  this.updateFeedback(updates);
-}
+  onLeaveFeedback = (option) => {
+    this.setState(prevState => ({[option]: prevState[option] + 1,}));
+  }
 
-updateFeedback = (updates) => {
-  this.setState(prevState => {
-  return Object.assign(prevState, updates)
-  })
-}
 
-countTotalFeedback = () => {
-  return (this.total = this.state.good + this.state.neutral + this.state.bad)}
+  countTotalFeedback = () => {
+    return (this.total = this.state.good + this.state.neutral + this.state.bad)}
 
-countPositiveFeedbackPercentage = () => {
-return ((this.total > 0)?(this.positiveFeedback = ( Math.ceil((this.state.good / this.total ) * 100 ))):0)
-}
+  countPositiveFeedbackPercentage = () => {
+  return ((this.total > 0)?(this.positiveFeedback = ( Math.ceil((this.state.good / this.total ) * 100 ))):0)
+  }
 
   render() {
     return (
       <div>
         <Section title={'Please leave feedback'}>
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback}/>      
+          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={(e) => this.onLeaveFeedback(e.target.textContent)}/>      
         </Section>
         <Section title={'Statistic'}>
-          {(Object.values(this.state).some(value => value > 0))?<Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>:<Notification message="There is no feedback"></Notification>}
+          {(Object.values(this.state).some(value => value > 0))?
+            <Statistics good={this.state.good} 
+                        neutral={this.state.neutral} 
+                        bad={this.state.bad} 
+                        total={this.countTotalFeedback()} 
+                        positivePercentage={this.countPositiveFeedbackPercentage()}/>:
+            <Notification message="There is no feedback"></Notification>}
         </Section>
       </div>
   )}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,12 @@ return (
     {'Bad': Number(Object.values({bad}).toString())},
     {'Total': Number(Object.values({total}).toString())},
     {'Positive feedback': Number(Object.values({positivePercentage}).toString())}]
-    .map((prop, index) => <ListItem key={index}><Text>{Object.keys(prop).toString()}: <Span>{Number(Object.values(prop).toString())}</Span></Text></ListItem>)}
+    .map((prop, index) => <ListItem key={index}>
+                            <Text>{Object.keys(prop).toString()}: 
+                              <Span>{Number(Object.values(prop).toString())}
+                              </Span>
+                            </Text>
+                          </ListItem>)}
   </StatisticsStyled>
 )};
 
